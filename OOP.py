76a3316987a3cfb42e4c0b09cfeff02556169772
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-03-30 00:18:26 +0530

    test

--- OOP.py
+++ OOP.py
@@ -6,14 +6,20 @@ class Animal:
     def __init__(self,name,age):
         self.name = name
         self.age = age
+    
 
 
 tiger = Animal("Tiger",50)
 
 cat = Animal("Cat",5)
+cat.numbers = 15
 
 print(tiger.__class__.spcies)
 print(cat.__class__.spcies)
 
 print(tiger.age)
 print(tiger.name)
+
+tr=cat.__dict__
+
+print(tr)
---
+++ test.py
@@ -0,0 +1,11 @@
+class fruits:
+    def __init__(self, price):
+        self.price = price
+obj=fruits(50)
+ 
+obj.quantity=10
+obj.bags=2
+ 
+print(obj.__dict__)
+print(obj.__class__)
+
