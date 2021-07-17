0457cb5b8ac6166b785378aa12472c7df4e50d39
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-07-18 00:33:12 +0530

    Update IndividualPage.dart
    1. Emoji picker COmplete
    2. Attachment Selector Complete

--- lib/Screens/IndividualPage.dart
+++ lib/Screens/IndividualPage.dart
@@ -171,7 +171,14 @@ class _IndividualPageState extends State<IndividualPage> {
                                       mainAxisSize: MainAxisSize.min,
                                       children: [
                                         IconButton(
-                                          onPressed: () {},
+                                          onPressed: () {
+                                            showModalBottomSheet(
+                                                backgroundColor:
+                                                    Colors.transparent,
+                                                context: context,
+                                                builder: (builder) =>
+                                                    bottomsheet());
+                                          },
                                           icon: Icon(Icons.attach_file),
                                         ),
                                         IconButton(
@@ -217,6 +224,78 @@ class _IndividualPageState extends State<IndividualPage> {
             return Future.value(false);
           },
         ),
+      ),
+    );
+  }
+
+  Widget bottomsheet() {
+    return Container(
+      height: 278,
+      width: MediaQuery.of(context).size.width,
+      child: Card(
+          margin: EdgeInsets.all(18),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    iconcreation(
+                        Icons.insert_drive_file, Colors.indigo, "Document"),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    iconcreation(Icons.camera_alt, Colors.pink, "Camera"),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    iconcreation(Icons.insert_photo, Colors.purple, "Gallery"),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    iconcreation(Icons.headset, Colors.orange, "Audio"),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    iconcreation(Icons.location_pin, Colors.teal, "Location"),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    iconcreation(Icons.person, Colors.blue, "Contact"),
+                  ],
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+
+  Widget iconcreation(IconData icon, Color color, String text) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: color,
+            child: Icon(
+              icon,
+              size: 29,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ],
       ),
     );
   }
