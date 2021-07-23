18c5a5c70e225d1078e28c0739d26e4c69beb756
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-07-23 19:54:04 +0530

    master

---
+++ lib/CustomUI/OwnMessageCard.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class OwnMessageCard extends StatelessWidget {
+  const OwnMessageCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width - 45,
+        ),
+        child: Card(
+          elevation: 1,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          color: Color(0xffdcf8c6),
+          margin: EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 5,
+          ),
+          child: Stack(children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 10,
+                right: 60,
+                top: 10,
+                bottom: 20,
+              ),
+              child: Text(
+                "Hey, Joy Sinha",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 4,
+              right: 10,
+              child: Row(
+                children: [
+                  Text(
+                    "20:58",
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Icon(
+                    Icons.done_all,
+                    size: 20,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/LoginScreen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/CustomUI/ButtonCard.dart';
+import 'package:my_app/Model/ChatModel.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  List<ChatModel> chatmodels = [
+    ChatModel(
+        name: "Joy Sinha",
+        icon: "person.svg",
+        isGroup: false,
+        time: "10.00",
+        currentMessage: "Hey JOY",
+        id: 1),
+    ChatModel(
+        name: "Debjyoti Das",
+        icon: "person.svg",
+        isGroup: false,
+        time: "03.00",
+        currentMessage: "Hey Buddy",
+        id: 2),
+    // ChatModel(
+    //     name: "Final Year Project",
+    //     icon: "groups.svg",
+    //     isGroup: true,
+    //     time: "08.00",
+    //     currentMessage: "Hey My groups"),
+    ChatModel(
+        name: "Avijit Samanta",
+        icon: "person.svg",
+        isGroup: false,
+        time: "03.00",
+        currentMessage: "Hey Joy, Work hard",
+        id: 3),
+    ChatModel(
+        name: "Maa",
+        icon: "person.svg",
+        isGroup: false,
+        time: "04.00",
+        currentMessage: "hi Beta",
+        id: 4)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: chatmodels.length,
+        itemBuilder: (context, index) => ButtonCard(
+          name: chatmodels[index].name,
+          icon: Icons.person,
+        ),
+      ),
+    );
+  }
+}
